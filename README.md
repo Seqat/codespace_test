fc14b4d1f604e5d3ebc0767ba9b3e8aa73128d84
Sedat Ali Zevit <37043095+Seqat@users.noreply.github.com>
2022-06-14 20:09:33 +0000

    First Day of the rust

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# codespace_test
+# Codespace_test: Every Giant learning path starts with a small code
 
+## Introduction
 This repository is a test bench for me to learn how to use Github Codespace.
 When I first started using Github Codespace, I was not sure how to use it.
 So I created this repository to test how to use it. So far, I started to
 learn Rust, and I am still learning Rust as I write this repositories README file.
 I hope that I can use Github Codespace to learn Rust.
+
+## 14/05/2022
+
+First, I started to learn the basics of Rust like how datatypes work or how to use the `println!` macro. Then, I try to understand how to take input from the user with the std::io::stdin() function. I think this iş will be a good way to learn how to use Github Codespace.
--- rust_test/src/main.rs
+++ rust_test/src/main.rs
@@ -1,9 +1,37 @@
+
+
 //My first code in Rust :D
 
+use std::io;
+
+
 fn main() {
-    
+
     //print "Hello, world!" with my style :D
     println!("Hello, world! This is my first code in Rust!");
+
+    // Task-1 : Create a variable called "x" and assign it the value of 5. 
+    // Then, print the value of x.     
     
-    println!("{} is my Lucky number",51);
+    let mut x:i32 = 5;
+
+    println!("TASK-1 \n {} is my Lucky number",x);
+    
+    x=10;
+    println!("Now, {} is my Lucky number",x);
+
+    // Task-2 : Create a String variable called "input" and take input from the user.
+    // Then, print the value of input.
+    
+    let mut input = String::new();
+
+    println!("TASK-2 \n Oh my! What's your number: ");
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+
+
+    println!("{} is the number came from our ancestors :D",input);
+
+
+
 }
